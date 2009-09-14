024ad191ae9b37ccbbd39325c477d76627189046
Peter Nedonosko <pnedonosko@exoplatform.com>
2009-09-14 13:29:17 +0000

    SWF-194: JCR core cmd files cleanup

--- component/core/TCK.cmd
+++ component/core/TCK.cmd
@@ -1,5 +1,3 @@
 @set MAVEN_OPTS=-Duser.language=en -Duser.region=us -Dexo.test.skip=true -Dexo.devtest.skip=true -Dexo.tck.skip=false -DforkMode=never -Dorg.exoplatform.jcr.monitor.jdbcMonitor %MAVEN_OPTS% 
 
 @start mvn clean install
-
-@rem start mvn -Dexo.tck.skip=false help:effective-pom
