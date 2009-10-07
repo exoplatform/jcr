2693b8f01be61eb0edae1c91091ad3b3af44d03c
Dimitri Baeli <dimitri.baeli@exoplatform.com>
2009-10-07 02:39:53 +0000

    EXOJCR-174: module.js file for jcr attached in the source code * same dependencies in the pom.xml * no hard-coded module version (support for maven release plugin)

---
+++ packaging/module/src/main/javascript/jcr.packaging.module.js
@@ -0,0 +1,42 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+  var core = params.core;
+  var ws = params.ws;
+
+  var module = {} ;
+  
+  module.version = "${project.version}" ; //
+  module.relativeMavenRepo =  "org/exoplatform/jcr" ;
+  module.relativeSRCRepo =  "jcr/trunk" ;
+  module.name =  "jcr" ;
+    
+  module.services = {}
+  module.services.jcr = 
+    new Project("org.exoplatform.jcr", "exo.jcr.component.core", "jar", module.version).
+    addDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.ext", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.webdav", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.ftp", "jar", module.version)) .
+    addDependency(core.component.documents) .
+    addDependency(new Project("jcr", "jcr", "jar", "1.0")).
+    addDependency(new Project("concurrent", "concurrent", "jar", "1.3.4")).
+    addDependency(new Project("javagroups", "jgroups-all", "jar", "2.5.2")).
+    addDependency(new Project("stax", "stax-api", "jar", "1.0")).
+//		addDependency(new Project("stax", "stax", "jar", "1.2.0")).
+		addDependency(new Project("org.apache.ws.commons","ws-commons-util","jar","1.0.1")).		
+    addDependency(new Project("org.apache.lucene", "lucene-core", "jar", "2.4.1")).
+    addDependency(new Project("org.apache.lucene", "lucene-spellchecker", "jar", "2.4.1")).
+    addDependency(new Project("org.apache.lucene", "lucene-memory", "jar", "2.4.1"));
+
+  module.frameworks = {}
+  module.frameworks.web = 
+    new Project("org.exoplatform.jcr", "exo.jcr.framework.web", "jar", module.version).  
+    addDependency(ws.rest).
+    addDependency(new Project("commons-chain", "commons-chain", "jar", "1.0"));
+
+  module.frameworks.command = new Project("org.exoplatform.jcr", "exo.jcr.framework.command", "jar", module.version).
+    addDependency(new Project("commons-fileupload", "commons-fileupload", "jar", "1.2.1")); 
+    
+  return module ;
+}
