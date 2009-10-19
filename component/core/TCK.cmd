3963f86d8ba9a94f64221dec5372a266904e74c2
Arnaud  Héritier <arnaud.heritier@exoplatform.com>
2009-10-19 12:40:20 +0000

    SWF-194 : Split unit tests, integration tests in different profiles

--- component/core/TCK.cmd
+++ component/core/TCK.cmd
@@ -1,3 +1,3 @@
-@set MAVEN_OPTS=-Duser.language=en -Duser.region=us -Dexo.test.skip=true -Dexo.devtest.skip=true -Dexo.tck.skip=false -DforkMode=never -Dorg.exoplatform.jcr.monitor.jdbcMonitor %MAVEN_OPTS% 
+@set MAVEN_OPTS=-Duser.language=en -Duser.region=us -Dmaven.test.skip=true -Dexo.devtest.skip=true -DforkMode=never -Dorg.exoplatform.jcr.monitor.jdbcMonitor %MAVEN_OPTS% 
 
-@start mvn clean install
+@start mvn clean install -Prun-its
