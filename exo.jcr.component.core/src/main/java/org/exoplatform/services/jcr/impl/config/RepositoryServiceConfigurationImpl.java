bab0f8bbb55f93f7340fc0dede222ab5651a5d59
Alexander Reshetnyak <alex.reshetnyak@exoplatform.com>
2010-01-21 17:12:07 +0000

    EXOJCR-381 : The create synchronization on method retain() in RepositoryServiceConfigurationImpl. This method maybe using at different threads.

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/config/RepositoryServiceConfigurationImpl.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/config/RepositoryServiceConfigurationImpl.java
@@ -148,7 +148,7 @@ public class RepositoryServiceConfigurationImpl extends RepositoryServiceConfigu
     * 
     * @throws RepositoryException
     */
-   public void retain() throws RepositoryException
+   public synchronized void retain() throws RepositoryException
    {
       try
       {
