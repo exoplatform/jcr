f78406882d78c26295caf6b9b06ca0e608c9fb4b
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2010-08-01 15:20:33 +0000

    EXOJCR-881

---
+++ exo.jcr.component.ext/src/test/resources/TestDependency.groovy
@@ -0,0 +1,14 @@
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+
+import dependencies.Dep1
+
+@Path("groovy-test-dependency")
+public class TestDependency {
+  
+  @GET
+  def method() {
+    return new Dep1().getName()
+  }
+  
+}
