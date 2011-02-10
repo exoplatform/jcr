dd4b2146a35e90f77b47a5b47c852f765fc73c5b
Anatoliy Bazko <abazko@exoplatform.com>
2011-02-10 10:04:24 +0000

    EXOJCR-1104: remove debug info

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCStorageConnection.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCStorageConnection.java
@@ -1028,9 +1028,7 @@ public abstract class JDBCStorageConnection extends DBConstants implements Works
       checkIfOpened();
       try
       {
-         long start = System.currentTimeMillis();
          ResultSet resultSet = findNodesAndProperties(offset, limit);
-         System.out.println("end  offset=" + offset + " time=" + (System.currentTimeMillis() - start));
 
          try
          {
