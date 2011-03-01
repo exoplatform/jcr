f8e9e2916b33aed93b442de11052d360aac6c46b
Anatoliy Bazko <abazko@exoplatform.com>
2011-03-01 21:30:45 +0000

    EXOJCR-1159: update module.js

--- packaging/module/src/main/javascript/jcr.packaging.module.js
+++ packaging/module/src/main/javascript/jcr.packaging.module.js
@@ -21,22 +21,22 @@ function getModule(params) {
     addDependency(core.component.documents) .
     addDependency(new Project("jcr", "jcr", "jar", "1.0")).
     addDependency(new Project("concurrent", "concurrent", "jar", "1.3.4")).
-    addDependency(new Project("jgroups", "jgroups", "jar", "2.6.13.GA")).
+    addDependency(new Project("jgroups", "jgroups", "jar", "2.11.1.Final")).
 //  addDependency(new Project("stax", "stax-api", "jar", "1.0")).
 //  addDependency(new Project("stax", "stax", "jar", "1.2.0")).
     addDependency(new Project("org.jboss.cache","jbosscache-core","jar","3.2.6.GA")).
     addDependency(new Project("jboss.jbossts","jbossjts","jar","4.6.1.GA")).
     addDependency(new Project("jboss.jbossts","jbossts-common","jar","4.6.1.GA")).
     addDependency(new Project("org.apache.ws.commons","ws-commons-util","jar","1.0.1")).
-    addDependency(new Project("org.apache.lucene", "lucene-core", "jar", "2.4.1")).
-    addDependency(new Project("org.apache.lucene", "lucene-spellchecker", "jar", "2.4.1")).
-    addDependency(new Project("org.apache.lucene", "lucene-memory", "jar", "2.4.1"));
+    addDependency(new Project("org.apache.lucene", "lucene-core", "jar", "2.9.4")).
+    addDependency(new Project("org.apache.lucene", "lucene-spellchecker", "jar", "2.9.4")).
+    addDependency(new Project("org.apache.lucene", "lucene-memory", "jar", "2.9.4"));
 
   module.frameworks = {}
   module.frameworks.web = 
     new Project("org.exoplatform.jcr", "exo.jcr.framework.web", "jar", module.version).  
     addDependency(ws.rest).
-    addDependency(new Project("commons-chain", "commons-chain", "jar", "1.0"));
+    addDependency(new Project("commons-chain", "commons-chain", "jar", "1.2"));
 
   module.frameworks.command = new Project("org.exoplatform.jcr", "exo.jcr.framework.command", "jar", module.version).
     addDependency(new Project("commons-fileupload", "commons-fileupload", "jar", "1.2.1")); 
