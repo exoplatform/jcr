3cf4681aafd022e8ac2eaa6eec9b8223f59aa963
Nicolas Filotto <nicolas.filotto@exoplatform.com>
2011-03-25 16:58:38 +0000

    EXOJCR-1260: Improved the configuration using EXOJCR-1268 and EXOJCR-1269

--- applications/product-patches/as/jetty/eXo.bat
+++ applications/product-patches/as/jetty/eXo.bat
@@ -1,1 +1,1 @@
-java -Djava.security.auth.login.config=jaas.conf -Dexo.profiles=%1 -Djava.net.preferIPv4Stack=true -jar start.jar
+java -Djava.security.auth.login.config=jaas.conf -Dexo.profiles=%1 -Djava.net.preferIPv4Stack=true -Dexo.jcr.parent.dir=. -jar start.jar
