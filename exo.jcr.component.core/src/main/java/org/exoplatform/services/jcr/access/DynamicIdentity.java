9740ff5ba5bfdc0ad528509ae5d146a0aa554dba
Trang Huyen Vu <trangvh@exoplatform.com>
2011-08-11 10:41:53 +0000

    JCR-1636: commit DynamicIdentity.java

---
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/access/DynamicIdentity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2003-2011 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.services.jcr.access;
+
+
+/**
+ * Created by The eXo Platform SAS.
+ * 
+ * <br/>Date: 2011
+ *
+ * @author <a href="mailto:alex.reshetnyak@exoplatform.com.ua">Alex Reshetnyak</a> 
+ * @version $Id: FakeUserIdentity.java 111 2011-11-11 11:11:11Z rainf0x $
+ */
+public class DynamicIdentity
+   extends SystemIdentity
+{
+   public static final String DYNAMIC = "__dynamic".intern();
+}
