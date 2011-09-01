dfd530a2b7da83227fde30e62c467a30f000a0af
Sergey Karpenko <sergey.karpenko@exoplatform.com>
2011-09-01 07:12:32 +0000

    EXOJCR-1239: patch from JCR-1571 committed (node index stored in lucene index)

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/query/lucene/NodeIndexer.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/query/lucene/NodeIndexer.java
@@ -217,6 +217,12 @@ public class NodeIndexer
          // unknown uri<->prefix mappings
       }
 
+      if (indexFormatVersion.getVersion() >= IndexFormatVersion.V4.getVersion())
+      {
+         doc.add(new Field(FieldNames.INDEX, Integer.toString(node.getQPath().getIndex()), Field.Store.YES,
+            Field.Index.NOT_ANALYZED_NO_NORMS));
+      }
+
       Collection<PropertyData> props = node.getChildPropertiesData();
       if (props == null)
       {
@@ -334,7 +340,7 @@ public class NodeIndexer
                   if (encProp == null)
                   {
                      encProp =
-                     (PropertyData)stateProvider.getItemData(node, new QPathEntry(Constants.JCR_ENCODING, 0),
+                        (PropertyData)stateProvider.getItemData(node, new QPathEntry(Constants.JCR_ENCODING, 0),
                            ItemType.PROPERTY);
                   }
 
