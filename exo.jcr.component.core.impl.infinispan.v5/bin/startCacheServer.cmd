5a67f857401ce32c37676b5543afc48bdc937481
Nicolas Filotto <nicolas.filotto@exoplatform.com>
2012-02-07 16:57:40 +0000

    EXOJCR-1682: Upgrade to ISPN 5.1.1.FINAL, doc, build and scripts reviewed (jcr)

--- exo.jcr.component.core.impl.infinispan.v5/bin/startCacheServer.cmd
+++ exo.jcr.component.core.impl.infinispan.v5/bin/startCacheServer.cmd
@@ -1,1 +1,9 @@
-java -Djava.net.preferIPv4Stack=true -Xms128m -Xmx512m -jar exo.jcr.component.core.impl.infinispan.v5-binary.jar %*
+ @echo off
+ setLocal EnableDelayedExpansion
+ set CACHE_SERVER_CP=".
+ for /R ./lib %%a in (*.jar) do (
+   set CACHE_SERVER_CP=!CACHE_SERVER_CP!;%%a
+ )
+ set CACHE_SERVER_CP=!CACHE_SERVER_CP!"
+
+java -Djava.net.preferIPv4Stack=true -Xms128m -Xmx512m -cp %CACHE_SERVER_CP% org.exoplatform.services.jcr.infinispan.CacheServer %*
--- exo.jcr.component.core.impl.infinispan.v5/bin/startCacheServer.sh
+++ exo.jcr.component.core.impl.infinispan.v5/bin/startCacheServer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-java -Djava.net.preferIPv4Stack=true -Xms128m -Xmx512m -jar exo.jcr.component.core.impl.infinispan.v5-binary.jar $*
+for i in lib/*.jar;do CACHE_SERVER_CP=$CACHE_SERVER_CP:$i;done
+java -Djava.net.preferIPv4Stack=true -Xms128m -Xmx512m -cp .$CACHE_SERVER_CP org.exoplatform.services.jcr.infinispan.CacheServer $*
