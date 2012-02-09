2f10c5494d0bbae09db4c5e68f394228af3a1d0d
Andrey Plotnikov <aplotnikov@exoplatform.com>
2012-02-09 15:00:02 +0000

    EXOJCR-1576: Adapted eXo JCR to GlusterFS

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCWorkspaceDataContainer.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCWorkspaceDataContainer.java
@@ -78,6 +78,7 @@ import org.picocontainer.Startable;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -1034,34 +1035,29 @@ public class JDBCWorkspaceDataContainer extends WorkspaceDataContainerBase imple
 
          if (wsConfig.getContainer().getValueStorages() != null)
          {
-            for (ValueStorageEntry valueStorage : wsConfig.getContainer().getValueStorages())
+            SecurityHelper.doPrivilegedExceptionAction(new PrivilegedExceptionAction<Void>()
             {
-               final File valueStorageDir = new File(valueStorage.getParameterValue(FileValueStorage.PATH));
-
-               SecurityHelper.doPrivilegedIOExceptionAction(new PrivilegedExceptionAction<Void>()
+               public Void run() throws IOException, RepositoryConfigurationException
                {
-                  public Void run() throws IOException
+                  for (ValueStorageEntry valueStorage : wsConfig.getContainer().getValueStorages())
                   {
+                     File valueStorageDir = new File(valueStorage.getParameterValue(FileValueStorage.PATH));
                      if (valueStorageDir.exists())
                      {
                         DirectoryHelper.removeDirectory(valueStorageDir);
                      }
-
-                     return null;
                   }
-               });
-            }
+
+                  return null;
+               }
+            });
          }
       }
-      catch (RepositoryConfigurationException e)
-      {
-         throw new BackupException(e);
-      }
-      catch (IOException e)
-      {
-         throw new BackupException(e);
-      }
       catch (DBCleanException e)
+      {
+         throw new BackupException(e);
+      }
+      catch (PrivilegedActionException e)
       {
          throw new BackupException(e);
       }
@@ -1111,14 +1107,14 @@ public class JDBCWorkspaceDataContainer extends WorkspaceDataContainerBase imple
          // backup value storage
          if (wsConfig.getContainer().getValueStorages() != null)
          {
-            for (final ValueStorageEntry valueStorage : wsConfig.getContainer().getValueStorages())
+            SecurityHelper.doPrivilegedExceptionAction(new PrivilegedExceptionAction<Void>()
             {
-               final File srcDir = new File(valueStorage.getParameterValue(FileValueStorage.PATH));
-
-               SecurityHelper.doPrivilegedIOExceptionAction(new PrivilegedExceptionAction<Void>()
+               public Void run() throws RepositoryConfigurationException, IOException
                {
-                  public Void run() throws IOException
+                  for (ValueStorageEntry valueStorage : wsConfig.getContainer().getValueStorages())
                   {
+                     File srcDir = new File(valueStorage.getParameterValue(FileValueStorage.PATH));
+
                      if (!srcDir.exists())
                      {
                         throw new IOException("Can't backup value storage. Directory " + srcDir.getName()
@@ -1129,22 +1125,21 @@ public class JDBCWorkspaceDataContainer extends WorkspaceDataContainerBase imple
                         File zipFile = new File(storageDir, "values-" + valueStorage.getId() + ".zip");
                         DirectoryHelper.compressDirectory(srcDir, zipFile);
                      }
-
-                     return null;
                   }
-               });
-            }
+                  return null;
+               }
+            });
          }
       }
       catch (IOException e)
       {
          throw new BackupException(e);
       }
-      catch (RepositoryConfigurationException e)
+      catch (RepositoryException e)
       {
          throw new BackupException(e);
       }
-      catch (RepositoryException e)
+      catch (PrivilegedActionException e)
       {
          throw new BackupException(e);
       }
