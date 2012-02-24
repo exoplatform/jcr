8efe4512dcd766f036793c35a482bd4fe1a6d380
Nikolay  Zamosenchuk <nikolay.zamosenchuk@exoplatform.com>
2012-02-24 09:46:37 +0000

    EXOJCR-364 : check if uri was already written to the output stream

--- exo.jcr.component.webdav/src/test/java/org/exoplatform/services/jcr/webdav/command/TestSearch.java
+++ exo.jcr.component.webdav/src/test/java/org/exoplatform/services/jcr/webdav/command/TestSearch.java
@@ -27,10 +27,22 @@ import org.exoplatform.services.rest.impl.ContainerResponse;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+import javax.jcr.Node;
 import javax.ws.rs.core.MediaType;
+import javax.xml.namespace.QName;
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.StartElement;
+
 
 /**
  * Created by The eXo Platform SAS. <br/>
@@ -46,8 +58,11 @@ public class TestSearch extends BaseStandaloneTest
 
    private final String fileContent = "TEST FILE CONTENT...";
 
-   private final String sql = "<D:searchrequest xmlns:D='DAV:'>" + "<D:sql>"
+   private final String basicSql = "<D:searchrequest xmlns:D='DAV:'>" + "<D:sql>"
       + "SELECT * FROM  nt:resource WHERE contains(*, 'TEST')" + "</D:sql>" + "</D:searchrequest>";
+
+   private final String pathSql = "<D:searchrequest xmlns:D='DAV:'>" + "<D:sql>"
+            + "SELECT * FROM nt:base WHERE jcr:path LIKE '/node[%]/%'" + "</D:sql>" + "</D:searchrequest>";
 
    public void testBasicSearch() throws Exception
    {
@@ -61,13 +76,35 @@ public class TestSearch extends BaseStandaloneTest
 
       InputStream inputStream = new ByteArrayInputStream(fileContent.getBytes());
       TestUtils.addContent(session, fileName, inputStream, defaultFileNodeType, MediaType.TEXT_PLAIN);
-      ContainerResponse response = service(WebDAVMethods.SEARCH, getPathWS(), "", null, sql.getBytes());
+      ContainerResponse response = service(WebDAVMethods.SEARCH, getPathWS(), "", null, basicSql.getBytes());
       assertEquals(HTTPStatus.MULTISTATUS, response.getStatus());
       SearchResultResponseEntity entity = (SearchResultResponseEntity)response.getEntity();
       ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
       entity.write(outputStream);
       String result = outputStream.toString();
       assertTrue(result.contains(fileName));
+   }
+
+   public void testPathSearch() throws Exception
+   {
+      Node testRoot = session.getRootNode().addNode("node", defaultFolderNodeType);
+      Node node = testRoot.addNode("addedNode", defaultFileNodeType);
+      node.addNode("jcr:content", "nt:resource");
+      Node content = node.getNode("jcr:content");
+      content.setProperty("jcr:mimeType", MediaType.TEXT_PLAIN);
+      content.setProperty("jcr:lastModified", Calendar.getInstance());
+      content.setProperty("jcr:data", new ByteArrayInputStream("Text".getBytes()));
+      session.save();
+      
+      ContainerResponse response = service(WebDAVMethods.SEARCH, getPathWS(), "", null, pathSql.getBytes());
+      SearchResultResponseEntity entity = (SearchResultResponseEntity)response.getEntity();
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      entity.write(outputStream);
+      outputStream.toByteArray();
+      List<String> found = parseNodeNames(outputStream.toByteArray());
+      System.out.println(found);
+      assertEquals(1, found.size());
+      
    }
 
    /**
@@ -86,7 +123,7 @@ public class TestSearch extends BaseStandaloneTest
       InputStream inputStream = new ByteArrayInputStream(fileContent.getBytes());
       TestUtils.addContent(session, decodedfileName, inputStream, defaultFileNodeType, MediaType.TEXT_PLAIN);
 
-      ContainerResponse response = service(WebDAVMethods.SEARCH, getPathWS(), "", null, sql.getBytes());
+      ContainerResponse response = service(WebDAVMethods.SEARCH, getPathWS(), "", null, basicSql.getBytes());
 
       // serialize response entity to string
       SearchResultResponseEntity entity = (SearchResultResponseEntity)response.getEntity();
@@ -109,5 +146,66 @@ public class TestSearch extends BaseStandaloneTest
    protected String getRepositoryName()
    {
       return null;
+   }
+   
+   /**
+    * Extracts names of nodes from response XML
+    * 
+    * @param data
+    * @return
+    * @throws XMLStreamException
+    * @throws FactoryConfigurationError
+    * @throws IOException
+    */
+   private List<String> parseNodeNames(byte[] data) throws XMLStreamException, FactoryConfigurationError, IOException
+   {
+      // flag, that notifies when parser is inside <D:displayname></D:displayname> 
+      boolean displayName = false;
+      //Set<String> nodes = new HashSet<String>();
+      List<String> nodes = new ArrayList<String>();
+      InputStream input = new ByteArrayInputStream(data);
+      XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(input);
+      QName name = QName.valueOf("{DAV:}href");
+      try
+      {
+         while (reader.hasNext())
+         {
+            int eventCode = reader.next();
+            switch (eventCode)
+            {
+               case StartElement.START_ELEMENT : {
+                  // if {DAV:}displayname opening element 
+                  if (reader.getName().equals(name))
+                  {
+                     displayName = true;
+                  }
+                  break;
+               }
+               case StartElement.CHARACTERS : {
+                  if (displayName)
+                  {
+                     // currently reader is inside <D:displayname>nodeName</D:displayname>
+                     // adding name to list if not empty
+                     String nodeName = reader.getText();
+                     if (nodeName != null && !nodeName.equals(""))
+                     {
+                        nodes.add(nodeName);
+                     }
+                  }
+                  break;
+               }
+               default : {
+                  displayName = false;
+                  break;
+               }
+            }
+         }
+      }
+      finally
+      {
+         reader.close();
+         input.close();
+      }
+      return new ArrayList<String>(nodes);
    }
 }
