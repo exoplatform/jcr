aafd5485b1c57b50272dba253d19dec609802507
Alexander Reshetnyak <alex.reshetnyak@exoplatform.com>
2012-03-12 14:20:30 +0000

    EXOJCR-1758 : The problem with eviction and stoping cache on repository stop was fixed.

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/lock/jbosscache/CacheableLockManagerImpl.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/lock/jbosscache/CacheableLockManagerImpl.java
@@ -33,8 +33,8 @@ import org.exoplatform.services.jcr.impl.dataflow.persistent.WorkspacePersistent
 import org.exoplatform.services.jcr.impl.storage.jdbc.DBConstants;
 import org.exoplatform.services.jcr.impl.storage.jdbc.DialectDetecter;
 import org.exoplatform.services.jcr.jbosscache.ExoJBossCacheFactory;
-import org.exoplatform.services.jcr.jbosscache.ExoJBossCacheFactory.CacheType;
 import org.exoplatform.services.jcr.jbosscache.PrivilegedJBossCacheHelper;
+import org.exoplatform.services.jcr.jbosscache.ExoJBossCacheFactory.CacheType;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.naming.InitialContextInitializer;
@@ -509,10 +509,18 @@ public class CacheableLockManagerImpl extends AbstractCacheableLockManager
 
       if (shareable)
       {
-         // The cache cannot be stopped since it can be shared so we evict the root node instead
-         cache.getNode(lockRoot).setResident(false);
-         cache.evict(lockRoot, true);
-         cache.getRegion(lockRoot, false).processEvictionQueues();
+         // The cache cannot be stopped since it can be shared so we remove the root node instead
+         try
+         {
+            cache.getInvocationContext().getOptionOverrides().setSuppressPersistence(true);
+            cache.getInvocationContext().getOptionOverrides().setCacheModeLocal(true);
+            cache.removeNode(lockRoot);
+         }
+         finally
+         {
+            cache.getInvocationContext().getOptionOverrides().setSuppressPersistence(false);
+            cache.getInvocationContext().getOptionOverrides().setCacheModeLocal(false);
+         }
       }
 
       try
