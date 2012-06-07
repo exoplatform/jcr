b31ee2bbc22ebafec0a6c7c76b9d81b17a4e9c1c
Alex Reshetnyak <areshetnyak@exoplatform.com>
2012-06-07 11:13:00 +0300

    EXOJCR-1795 : Unit tests was added.

--- exo.jcr.component.core/src/test/java/org/exoplatform/services/jcr/impl/dataflow/TestOptimizationChangesLog.java
+++ exo.jcr.component.core/src/test/java/org/exoplatform/services/jcr/impl/dataflow/TestOptimizationChangesLog.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2003-2012 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
 package org.exoplatform.services.jcr.impl.dataflow;
 
 import java.util.List;
@@ -10,6 +26,14 @@ import org.exoplatform.services.jcr.dataflow.ItemState;
 import org.exoplatform.services.jcr.dataflow.TransactionChangesLog;
 import org.exoplatform.services.jcr.impl.dataflow.serialization.TesterItemsPersistenceListener;
 
+/**
+ * Created by The eXo Platform SAS.
+ * 
+ * <br/>Date: 2012
+ *
+ * @author <a href="mailto:alex.reshetnyak@exoplatform.com.ua">Alex Reshetnyak</a> 
+ * @version $Id: TestOptimizationChangesLog.java 111 2011-11-11 11:11:11Z rainf0x $
+ */
 public class TestOptimizationChangesLog extends JcrImplBaseTest
 {
    /**
