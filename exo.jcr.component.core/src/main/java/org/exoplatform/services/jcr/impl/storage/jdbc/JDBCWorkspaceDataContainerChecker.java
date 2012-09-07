2ed709f6e00faf5dc2e3ca192c454df01d1dd825
Anatoliy Bazko <abazko@exoplatform.com>
2012-09-07 09:23:57 +0300

    JCR-1843: query improved

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCWorkspaceDataContainerChecker.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCWorkspaceDataContainerChecker.java
@@ -30,7 +30,6 @@ import org.exoplatform.services.jcr.impl.Constants;
 import org.exoplatform.services.jcr.impl.checker.DummyRepair;
 import org.exoplatform.services.jcr.impl.checker.EarlierVersionsRemover;
 import org.exoplatform.services.jcr.impl.checker.InspectionQuery;
-import org.exoplatform.services.jcr.impl.checker.InspectionQueryFilteredMultivaluedProperties;
 import org.exoplatform.services.jcr.impl.checker.InspectionReport;
 import org.exoplatform.services.jcr.impl.checker.NodeRemover;
 import org.exoplatform.services.jcr.impl.checker.PropertyRemover;
@@ -496,40 +495,30 @@ public class JDBCWorkspaceDataContainerChecker
             "Items that do not have parent nodes", new RootAsParentAssigner(jdbcDataContainer
                .getConnectionFactory())));
 
-      /*   
-       * This checking need because In DB2 database P_MULTIVALUED fields type is integer
-       * but In HSQL this fields type is boolean. Our code working with both database. 
-       * There are differences in data type matching between java and DB2 and some problems 
-       * related to dates or numbers or decimals. We have some problem when We try 
-       * execute next query because query use P_MULTIVALUED field.
-       */
-      if (jdbcDataContainer.dbDialect.equals(DBConstants.DB_DIALECT_DB2)
-         || jdbcDataContainer.dbDialect.equals(DBConstants.DB_DIALECT_DB2V8))
+      String statement =
+         jdbcDataContainer.multiDb
+            ? "select * from JCR_MITEM P where P.I_CLASS=2 and P.P_MULTIVALUED=? and NOT EXISTS(select * from JCR_MVALUE V "
+               + "where V.PROPERTY_ID=P.ID)" : "select * from JCR_SITEM P where P.CONTAINER_NAME='"
+               + jdbcDataContainer.containerName + "' and P.I_CLASS=2"
+               + " and P.P_MULTIVALUED=? and NOT EXISTS(select * from JCR_SVALUE V where V.PROPERTY_ID=P.ID)";
+
+      if (jdbcDataContainer.dbDialect.equalsIgnoreCase(DBConstants.DB_DIALECT_PGSQL))
       {
-         itemsInspectionQuery
-            .add(new InspectionQuery(
-               jdbcDataContainer.multiDb
-                  ? "select * from JCR_MITEM P where P.I_CLASS=2 and P.P_MULTIVALUED=0 and NOT EXISTS(select * from JCR_MVALUE V "
-                     + "where V.PROPERTY_ID=P.ID)" : "select * from JCR_SITEM P where P.CONTAINER_NAME='"
-                     + jdbcDataContainer.containerName + "' and P.I_CLASS=2"
-                     + " and P.P_MULTIVALUED=0 and NOT EXISTS( select * from JCR_SVALUE V where V.PROPERTY_ID=P.ID)",
-               new String[]{DBConstants.COLUMN_ID, DBConstants.COLUMN_PARENTID, DBConstants.COLUMN_NAME},
-               "A node that has a single valued properties with nothing declared in the VALUE table.",
-               new PropertyRemover(jdbcDataContainer.getConnectionFactory(), nodeTypeManager)));
+         statement = statement.replace("?", "'f'");
+      }
+      else if (jdbcDataContainer.dbDialect.equalsIgnoreCase(DBConstants.DB_DIALECT_HSQLDB))
+      {
+         statement = statement.replace("?", "FALSE");
       }
       else
       {
-         itemsInspectionQuery
-            .add(new InspectionQueryFilteredMultivaluedProperties(
-               jdbcDataContainer.multiDb
-                  ? "select * from JCR_MITEM P where P.I_CLASS=2 and P.P_MULTIVALUED=? and NOT EXISTS(select * from JCR_MVALUE V "
-                     + "where V.PROPERTY_ID=P.ID)" : "select * from JCR_SITEM P where P.CONTAINER_NAME='"
-                     + jdbcDataContainer.containerName + "' and P.I_CLASS=2"
-                     + " and P.P_MULTIVALUED=? and NOT EXISTS( select * from JCR_SVALUE V where V.PROPERTY_ID=P.ID)",
-               new String[]{DBConstants.COLUMN_ID, DBConstants.COLUMN_PARENTID, DBConstants.COLUMN_NAME},
-               "A node that has a single valued properties with nothing declared in the VALUE table.",
-               new PropertyRemover(jdbcDataContainer.getConnectionFactory(), nodeTypeManager)));
+         statement = statement.replace("?", "0");
       }
+
+      itemsInspectionQuery.add(new InspectionQuery(statement, new String[]{DBConstants.COLUMN_ID,
+         DBConstants.COLUMN_PARENTID, DBConstants.COLUMN_NAME},
+         "A node that has a single valued properties with nothing declared in the VALUE table.", new PropertyRemover(
+            jdbcDataContainer.getConnectionFactory(), nodeTypeManager)));
 
       itemsInspectionQuery.add(new InspectionQuery(jdbcDataContainer.multiDb
          ? "select * from JCR_MITEM N where N.I_CLASS=1 and NOT EXISTS "
@@ -552,7 +541,6 @@ public class JDBCWorkspaceDataContainerChecker
             jdbcDataContainer.multiDb)));
 
       // The differences in the queries by DB dialect.
-      String statement;
       if (jdbcDataContainer.dbDialect.equalsIgnoreCase(DBConstants.DB_DIALECT_SYBASE))
       {
          statement =
