1af35f96b1713ff91a5be30a1bd229df71837a65
Andrey Plotnikov <aplotnikov@exoplatform.com>
2012-10-01 12:12:28 +0300

    JCR-1904: Added log4j.properties file in tomcat, jonas and jetty servers

--- applications/product-patches/as/jetty/eXo.bat
+++ applications/product-patches/as/jetty/eXo.bat
@@ -1,1 +1,1 @@
-java -Djava.security.auth.login.config=jaas.conf -Dexo.profiles=@exo.deploy.profile@ -Djava.net.preferIPv4Stack=true -Dexo.jcr.parent.dir=. -jar start.jar
+java -Dlog4j.configuration=file:./resources/log4j.properties -Djava.security.auth.login.config=jaas.conf -Dexo.profiles=@exo.deploy.profile@ -Djava.net.preferIPv4Stack=true -Dexo.jcr.parent.dir=. -jar start.jar
