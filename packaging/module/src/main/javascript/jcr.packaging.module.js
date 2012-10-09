9d23bc477b76fa5e27d9f82dcb5ab6b631e855fa
Anatoliy Bazko <abazko@exoplatform.com>
2012-10-09 09:44:14 +0300

    JCR-1927: fix

--- packaging/module/src/main/javascript/jcr.packaging.module.js
+++ packaging/module/src/main/javascript/jcr.packaging.module.js
@@ -19,7 +19,7 @@ function getModule(params) {
     addDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.webdav", "jar", module.version)).
     addDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.ftp", "jar", module.version)) .
     addDependency(core.component.documents) .
-    addDependency(new Project("jcr", "jcr", "jar", "1.0")).
+    addDependency(new Project("javax.jcr", "jcr", "jar", "1.0")).
     addDependency(new Project("concurrent", "concurrent", "jar", "1.3.4")).
     addDependency(new Project("org.jgroups", "jgroups", "jar", "2.6.20.Final")).
     addDependency(new Project("org.jboss.cache","jbosscache-core","jar","3.2.7.GA")).
