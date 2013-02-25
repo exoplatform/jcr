0178d3528eedee9ab75c224adb3de02482b86764
aymen <aymen.boughzela@exoplatform.com>
2013-02-25 11:06:26 +0100

    JCR-2101 : ObservationManager can contain unregistered listeners

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/observation/ActionLauncher.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/observation/ActionLauncher.java
@@ -82,7 +82,19 @@ public class ActionLauncher implements ItemsPersistenceListener
       {
 
          EventListener listener = eventListeners.nextEventListener();
+         if (listener == null) {
+              if (LOG.isDebugEnabled()) {
+                  LOG.debug("The listener has probably been removed thus it will be skipped");
+              }
+              continue;
+         }
          ListenerCriteria criteria = observationRegistry.getListenerFilter(listener);
+         if (criteria == null) {
+              if (LOG.isDebugEnabled()) {
+                  LOG.debug("The criteria of the listener could not be found thus it will be skipped");
+              }
+              continue;
+         }
 
          EntityCollection events = new EntityCollection();
 
