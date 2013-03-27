60c2887841afedb8df9ac558ebdbdf827d19b10f
aymen <aymen.boughzela@exoplatform.com>
2013-03-27 09:35:25 +0100

    JCR-2135 : Default maxClauseCount (1024) is exceeded with lucene BooleanQuery

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/query/SearchManager.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/core/query/SearchManager.java
@@ -24,6 +24,7 @@ import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.WildcardQuery;
 import org.exoplatform.commons.utils.ClassLoading;
 import org.exoplatform.commons.utils.PrivilegedFileHelper;
+import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.commons.utils.SecurityHelper;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.configuration.ConfigurationManager;
@@ -254,6 +255,37 @@ public class SearchManager implements Startable, MandatoryItemsPersistenceListen
    private final ExoContainerContext ctx;
 
    private String hotReindexingState = "not stated";
+
+   /**
+    * Name of max clause count property.
+    */
+
+   private static final String LUCENE_BOOLEAN_QUERY_MAX_CLAUSE_COUNT = "org.apache.lucene.maxClauseCount";
+
+   /**
+    * Maximum number of clauses permitted per BooleanQuery.
+    */
+   private static final int MAX_CLAUSE_COUNT;
+
+   static
+   {
+      String max = PropertyManager.getProperty(LUCENE_BOOLEAN_QUERY_MAX_CLAUSE_COUNT);
+      int value = Integer.MAX_VALUE;
+      if (max != null)
+      {
+         try
+         {
+            value = Integer.valueOf(max);
+         }
+         catch (NumberFormatException e)
+         {
+            LOG.warn("The value of the property '" + LUCENE_BOOLEAN_QUERY_MAX_CLAUSE_COUNT
+               + "' must be an integer, the default value will be used.");
+         }
+      }
+      MAX_CLAUSE_COUNT = value;
+      BooleanQuery.setMaxClauseCount(MAX_CLAUSE_COUNT);
+   }
 
    public SearchManager(ExoContainerContext ctx, WorkspaceEntry wEntry, RepositoryEntry rEntry,
       RepositoryService rService, QueryHandlerEntry config, NamespaceRegistryImpl nsReg, NodeTypeDataManager ntReg,
