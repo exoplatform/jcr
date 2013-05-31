5e94d2ba030d47aadeabaa8767a26f4c02e4ac87
aymen <aymen.boughzela@exoplatform.com>
2013-05-31 10:21:38 +0100

    JCR-2180 : Make the JCA Resource Adapter work on JBoss AS 7

--- exo.jcr.connectors.jca/src/main/java/org/exoplatform/connectors/jcr/adapter/SessionFactory.java
+++ exo.jcr.connectors.jca/src/main/java/org/exoplatform/connectors/jcr/adapter/SessionFactory.java
@@ -18,6 +18,7 @@
  */
 package org.exoplatform.connectors.jcr.adapter;
 
+import java.io.Serializable;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.resource.cci.ConnectionFactory;
@@ -29,7 +30,7 @@ import javax.resource.cci.ConnectionFactory;
  * @version $Id$
  *
  */
-public interface SessionFactory
+public interface SessionFactory  extends Serializable
 {
    /**
     * Get a JCR session corresponding to the repository
