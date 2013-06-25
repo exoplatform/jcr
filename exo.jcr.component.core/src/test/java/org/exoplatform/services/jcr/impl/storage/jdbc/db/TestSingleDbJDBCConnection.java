dfefd063a153e039cc1b53ad420c4562c41857cd
Nicolas Filotto <nfilotto@exoplatform.com>
2013-06-25 18:31:22 +0000

    JCR-2189: removed a useless st.close

--- exo.jcr.component.core/src/test/java/org/exoplatform/services/jcr/impl/storage/jdbc/db/TestSingleDbJDBCConnection.java
+++ exo.jcr.component.core/src/test/java/org/exoplatform/services/jcr/impl/storage/jdbc/db/TestSingleDbJDBCConnection.java
@@ -107,7 +107,6 @@ public class TestSingleDbJDBCConnection extends JDBCConnectionTestBase
          st.executeUpdate("drop table JCR_SREF");
          st.executeUpdate("drop table JCR_SVALUE");
          st.executeUpdate("drop table JCR_SITEM");
-         st.close();
       }
       catch (SQLException se)
       {
