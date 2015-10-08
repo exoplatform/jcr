a222cb7a0c366b3bc24a0ed3eb167355143c9132
trangvh <trangvh@exoplatform.com>
2015-10-08 18:54:47 +0700

    JCR-2405: Fix a typo in log of JDBCWorkspaceDataContainer

--- exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCWorkspaceDataContainer.java
+++ exo.jcr.component.core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/JDBCWorkspaceDataContainer.java
@@ -891,7 +891,7 @@ public class JDBCWorkspaceDataContainer extends WorkspaceDataContainerBase imple
                         if (!srcDir.exists())
                         {
                            throw new IOException("Can't backup value storage. Directory " + srcDir.getName()
-                              + " doesn't exists");
+                              + " doesn't exist");
                         }
                         else
                         {
