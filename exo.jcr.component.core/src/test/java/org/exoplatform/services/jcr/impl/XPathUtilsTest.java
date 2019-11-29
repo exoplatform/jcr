accdb75d04d392c8b63b034d0a5d88e69ffd0a79
Boubaker Khanfir <bkhanfir@exoplatform.com>
2019-11-29 20:10:38 +0100

    Cleanup Commons from JCR

---
+++ exo.jcr.component.core/src/test/java/org/exoplatform/services/jcr/impl/XPathUtilsTest.java
@@ -0,0 +1,59 @@
+/***************************************************************************
+ * Copyright (C) 2003-2014 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+package org.exoplatform.services.jcr.impl;
+
+import org.exoplatform.services.jcr.impl.util.XPathUtils;
+
+import junit.framework.TestCase;
+
+public class XPathUtilsTest extends TestCase {
+
+  public void testNull () {
+    assertNull(XPathUtils.escapeIllegalXPathName(null));
+  }
+  
+  public void testAtSign () {
+    String hexString;
+    hexString = XPathUtils.escapeIllegalXPathName("@testme");
+    assertEquals("At sign is not convertible",hexString,"_x0040_testme");
+    hexString = XPathUtils.escapeIllegalXPathName("/@testme");
+    assertEquals("At sign is not convertible",hexString,"/_x0040_testme");
+    hexString = XPathUtils.escapeIllegalXPathName("/alex.goodman@testme");
+    assertEquals("At sign is not convertible",hexString,"/alex.goodman_x0040_testme");
+  }
+  
+  public void testNumeric() {
+    String hexString;
+    hexString = XPathUtils.escapeIllegalXPathName("123456789");
+    assertEquals("Cannot convert path starting with numberic",hexString ,"_x0031_23456789");
+    hexString = XPathUtils.escapeIllegalXPathName("/123456789");
+    assertEquals("Cannot convert path starting with numberic",hexString ,"/_x0031_23456789");
+    hexString = XPathUtils.escapeIllegalXPathName("/12345/6789");
+    assertEquals("Cannot convert path starting with numberic",hexString ,"/_x0031_2345/_x0036_789");
+  }
+  
+  public void testEscapeIllegalSQLName() {
+    assertNull(XPathUtils.escapeIllegalSQLName(null));
+    assertEquals("", XPathUtils.escapeIllegalSQLName(""));
+    //
+    assertEquals("''", XPathUtils.escapeIllegalSQLName("'"));
+    assertEquals("It''s a foo", XPathUtils.escapeIllegalSQLName("It's a foo"));
+    assertEquals("/a/b''/c", XPathUtils.escapeIllegalSQLName("/a/b'/c"));
+  }
+}
--- exo.jcr.component.ext/src/main/java/org/exoplatform/services/jcr/ext/app/SessionProviderService.java
+++ exo.jcr.component.ext/src/main/java/org/exoplatform/services/jcr/ext/app/SessionProviderService.java
@@ -18,41 +18,86 @@
  */
 package org.exoplatform.services.jcr.ext.app;
 
+import javax.jcr.RepositoryException;
+
+import org.exoplatform.container.*;
 import org.exoplatform.container.spi.DefinitionByType;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 
 /**
  * Created by The eXo Platform SAS . Session providers holder component
  * 
  * @author Gennady Azarenkov
- * @version $Id: SessionProviderService.java 13869 2008-05-05 08:40:10Z pnedonosko $
+ * @version $Id: SessionProviderService.java 13869 2008-05-05 08:40:10Z
+ *          pnedonosko $
  */
 @DefinitionByType(type = ThreadLocalSessionProviderService.class)
-public interface SessionProviderService
-{
+public interface SessionProviderService {
 
-   /**
-    * @param key
-    * @param sessionProvider
-    */
-   void setSessionProvider(Object key, SessionProvider sessionProvider);
+  /**
+   * @param key
+   * @param sessionProvider
+   */
+  void setSessionProvider(Object key, SessionProvider sessionProvider);
 
-   /**
-    * @param key
-    * @return session provider
-    */
-   SessionProvider getSessionProvider(Object key);
+  /**
+   * @param key
+   * @return session provider
+   */
+  SessionProvider getSessionProvider(Object key);
 
-   /**
-    * @param key
-    * @return system session provider
-    */
-   SessionProvider getSystemSessionProvider(Object key);
+  /**
+   * @param key
+   * @return system session provider
+   */
+  SessionProvider getSystemSessionProvider(Object key);
 
-   /**
-    * Removes the session provider
-    * 
-    * @param key
-    */
-   void removeSessionProvider(Object key);
+  /**
+   * Removes the session provider
+   * 
+   * @param key
+   */
+  void removeSessionProvider(Object key);
+
+  /**
+   * Gets the system session provider.
+   *
+   * @return the system session provider
+   */
+  public static SessionProvider getSystemSessionProvider() {
+    SessionProviderService sessionProviderService = getSessionProviderService();
+    return sessionProviderService.getSystemSessionProvider(null);
+  }
+
+  /**
+   * Get the current repository
+   *
+   * @return the current manageable repository
+   */
+  public static ManageableRepository getRepository() {
+    try {
+      return getRepositoryService().getCurrentRepository();
+    } catch (RepositoryException e) {
+      return null;
+    }
+  }
+
+  public static SessionProviderService getSessionProviderService() {
+    ExoContainer container = ExoContainerContext.getCurrentContainer();
+    if (container == null || container.getComponentInstanceOfType(SessionProviderService.class) == null) {
+      container = PortalContainer.getInstance();
+    }
+    return container.getComponentInstanceOfType(SessionProviderService.class);
+  }
+
+  public static RepositoryService getRepositoryService() {
+    ExoContainer container = ExoContainerContext.getCurrentContainer();
+    if (container == null || container.getComponentInstanceOfType(RepositoryService.class) == null) {
+      container = PortalContainer.getInstance();
+    }
+    return container.getComponentInstanceOfType(RepositoryService.class);
+  }
+
 }
